8f10a86223c22bccb72f5ab5b54c0521cb253b55
albertostizzoli <albertostizzoli@yahoo.it>
2024-05-31 08:23:52 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<h1 class="text-center bg-danger">Hello world</h1>
+<i class="fa-brands fa-github fa-3x"></i>
+
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
